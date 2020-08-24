d038e47d7e7567a07fb1a4b6fd7bd0a5ff6e6f11
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-08-24 18:34:35 +0530

    Model schema created

---
+++ server/models/Customer.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const customerSchema = new Schema({
+    name:{type:String},
+    email_id:{type:String},
+    phone:{type:String},
+    password:{type:String},
+    booking_current:[{
+        type:Schema.Types.ObjectId,
+        ref:'bookings'
+    }],
+    booking_history:[{
+        type:Schema.Types.ObjectId,
+        ref:'bookings'
+    }]
+})
+
+const Customer = mongoose.model('customers',customerSchema)
+
+module.exports = Customer
