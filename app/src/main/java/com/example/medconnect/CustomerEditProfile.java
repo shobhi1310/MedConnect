2cbbd241d69af0135f181ebd63c996dd1c526e87
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-08-24 23:21:52 +0530

    customer edit profile page integrated

---
+++ app/src/main/java/com/example/medconnect/CustomerEditProfile.java
@@ -0,0 +1,10 @@
+package com.example.medconnect;
+
+import android.os.Bundle;
+
+public class CustomerEditProfile extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState, R.layout.customer_editprofile);
+    }
+}
