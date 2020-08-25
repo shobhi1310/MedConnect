35fb037c9aa9f81cca09eb1f6a8d95538f8d0a4c
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-08-25 18:33:08 +0530

    FAB created for shop owner page

--- app/src/main/java/com/example/medconnect/ShopOwnerHome.java
+++ app/src/main/java/com/example/medconnect/ShopOwnerHome.java
@@ -6,11 +6,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
