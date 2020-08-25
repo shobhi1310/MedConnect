f7a9f054e5ef51da04595145cb33c130b3129f48
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-08-26 00:08:33 +0530

    bug fix in routes till now

--- server/routes/medicine.js
+++ server/routes/medicine.js
@@ -5,7 +5,7 @@ router.route('/fetch/:text').get(async (req,res)=>{
     const query = req.params.text
     let medicines
     try {
-        medicines = await medicineModel.find({name:`/^${query}/`},{_id:1,name:1,manufacturer:1,strength:1}).limit(20);
+        medicines = await medicineModel.find({name:new RegExp('^'+query,'i')},{_id:1,name:1,manufacturer:1,strength:1}).limit(20);
         res.json(medicines);
     } catch (error) {
         res.json(error);
