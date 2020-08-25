7dfbbbcf413ed23fcd904ac5f0b9e016b3d37c7c
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-08-26 00:00:21 +0530

    routes for medicine created

--- server/index.js
+++ server/index.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose')
-const connection = require('./db/mongoose')
+// const connection = require('./db/mongoose')
 
 require('dotenv').config();
 
 const app = express();
+const medicineRouter = require('./routes/medicine')
+
 
 app.use(cors);
 app.use(express.json());
-
+app.use('/medicine',medicineRouter);
 
 const port = process.env.PORT || 5000
 
-let gfs
+const uri = process.env.URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
 connection.once('open', () => {
-  gfs = new mongoose.mongo.GridFSBucket(connection.db, { bucketName: 'uploads' })
-  app.locals.gfs = gfs
+  console.log("MongoDB database connection established successfully");
+})
+
+
+// let gfs
+// connection.once('open', () => {
+//   gfs = new mongoose.mongo.GridFSBucket(connection.db, { bucketName: 'uploads' })
+//   app.locals.gfs = gfs
+// })
+
+app.get('/',(req,res)=>{
+  console.log("hello!!");
+  res.json("Hello!!");
 })
 
 app.listen(port,()=>{
--- server/routes/medicine.js
+++ server/routes/medicine.js
@@ -1,4 +1,27 @@
 const router = require('express').Router()
 const medicineModel = require('../models/Medicine.model')
 
+router.route('/fetch/:text').get(async (req,res)=>{
+    const query = req.params.text
+    let medicines
+    try {
+        medicines = await medicineModel.find({name:`/^${query}/`},{_id:1,name:1,manufacturer:1,strength:1}).limit(20);
+        res.json(medicines);
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+router.route('/:id').get(async (req,res)=>{
+    console.log("here");
+    const id = req.params.id
+    let medicine
+    try {
+        medicine = await medicineModel.findById(id)
+        res.json(medicine)
+    } catch (error) {
+        res.json(error)
+    }
+})
+
 module.exports = router
