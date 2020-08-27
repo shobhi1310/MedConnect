3241948c5b17c1430f80d0ed1039a38012ff8f77
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-08-27 22:40:53 +0530

    seekbar quantized

--- app/src/main/java/com/example/medconnect/BookMedicine.java
+++ app/src/main/java/com/example/medconnect/BookMedicine.java
@@ -5,10 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class BookMedicine extends AppCompatActivity {
 
+    private int timeRange;
+    private ArrayList<Integer> time = new ArrayList<Integer>(){
+        {add(15); add(30); add(45); add(60); add(90); add(120);}
+    };
+    private Button book;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +55,33 @@ public class BookMedicine extends AppCompatActivity {
         mfg_name.setText(medicineItem.getManufacturer());
         medicine_weight.setText(medicineItem.getWeight());
 
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        book = (Button) findViewById(R.id.book_button);
+        if(seekBar!=null){
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
 
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+//                    Toast.makeText(BookMedicine.this, "Current value is " + seekBar.getProgress(), Toast.LENGTH_SHORT).show();
+                    timeRange = seekBar.getProgress();
+                }
+            });
+        }
+
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(BookMedicine.this, "Current value is " + time.get(timeRange), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
