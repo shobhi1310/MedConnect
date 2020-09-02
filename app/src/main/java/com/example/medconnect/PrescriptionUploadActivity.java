1a0519c90e47de2f9360259f313e0c56a2e1a11e
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-09-02 20:08:52 +0530

    upload prescriptions page

---
+++ app/src/main/java/com/example/medconnect/PrescriptionUploadActivity.java
@@ -0,0 +1,80 @@
+package com.example.medconnect;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PrescriptionUploadActivity extends AppCompatActivity {
+    Button upload;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prescription_upload);
+        upload = findViewById(R.id.uploadPrescription);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void selectPdf(){
+        //to offer user to select file from manager
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT); //to fetch files
+        startActivityForResult(intent,86);
+    }
+    private void checkStoragePermission(){
+        if (ContextCompat.checkSelfPermission(PrescriptionUploadActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            //permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(PrescriptionUploadActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Requires storage permission")
+                        .setMessage("you have to give this permission to access the feature")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(PrescriptionUploadActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},9);
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+            else {
+                ActivityCompat.requestPermissions(PrescriptionUploadActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},9);
+            }
+        }
+        else{
+            selectPdf();
+        }
+    }
+}
