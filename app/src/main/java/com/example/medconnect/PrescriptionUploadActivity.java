bf465bd54787d4e3d8dcab1cd6cac55c50df4fd1
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-09-03 19:54:43 +0530

    prescription upload concept changed

--- app/src/main/java/com/example/medconnect/PrescriptionUploadActivity.java
+++ app/src/main/java/com/example/medconnect/PrescriptionUploadActivity.java
@@ -2,19 +2,27 @@ package com.example.medconnect;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.loader.content.CursorLoader;
 
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,9 +30,11 @@ public class PrescriptionUploadActivity extends AppCompatActivity {
     Button select;
     TextView file;
     Button upload;
+    ImageView imagePreview;
     Integer quantity = 0;
     SelectShopCard shop=null;
     MedicineItem medicineItem=null;
+    String path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,9 +42,17 @@ public class PrescriptionUploadActivity extends AppCompatActivity {
         select = findViewById(R.id.selectPrescription);
         file = findViewById(R.id.fileName);
         upload = findViewById(R.id.uploadPrescription);
+        imagePreview = findViewById(R.id.previewImage);
         Intent intent= getIntent();
+        TextView toolbar_title = findViewById(R.id.toolbar_title);
+        toolbar_title.setText("Upload Prescription");
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar= getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-
+        upload.setVisibility(View.INVISIBLE);
+        file.setVisibility(View.INVISIBLE);
 
         if(intent.getExtras()!=null){
             shop= (SelectShopCard) intent.getSerializableExtra("Shop");
@@ -72,15 +90,20 @@ public class PrescriptionUploadActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 86 && resultCode == RESULT_OK && data != null) {
             //pdfUri = data.getData(); // return the uri of selected file
-            file.setText("A file is selected : " + data.getData().getLastPathSegment());
+            imagePreview.setImageURI(data.getData());
+            upload.setVisibility(View.VISIBLE);
+//            file.setVisibility(View.VISIBLE);
+//            path = getPathFromURI(data.getData());
+//            Log.d("ImagePath",path);
+//            file.setText(path);
         } else {
             Toast.makeText(PrescriptionUploadActivity.this, "please select a file..", Toast.LENGTH_SHORT).show();
         }
     }
-    private void selectPdf(){
+    private void selectImage(){
         //to offer user to select file from manager
         Intent intent = new Intent();
-        intent.setType("application/pdf");
+        intent.setType("image/jpeg");
         intent.setAction(Intent.ACTION_GET_CONTENT); //to fetch files
         startActivityForResult(intent,86);
     }
@@ -112,7 +135,19 @@ public class PrescriptionUploadActivity extends AppCompatActivity {
             }
         }
         else{
-            selectPdf();
+            selectImage();
         }
+    }
+    private String getPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getApplicationContext(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        finish();
+        return true;
     }
 }
