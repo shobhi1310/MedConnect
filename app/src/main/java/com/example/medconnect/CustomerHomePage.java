eec40022a1bacf3215e00339cc50476cf84dc40c
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-09-05 20:13:56 +0530

    nav menu disabled for customer

--- app/src/main/java/com/example/medconnect/CustomerHomePage.java
+++ app/src/main/java/com/example/medconnect/CustomerHomePage.java
@@ -17,6 +17,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -50,7 +51,8 @@ public class CustomerHomePage extends BaseActivity {
     private ArrayList<CustomerBookingHistoryCard> orders;
     boolean doubleBackToExitPressedOnce = false;
     private RequestQueue queue;
-
+    private MenuItem item;
+    private NavigationView nav;
     private ProgressBar spinner;
 
     SwipeRefreshLayout swipe;
@@ -67,7 +69,8 @@ public class CustomerHomePage extends BaseActivity {
         swipe.setColorSchemeColors(R.color.background);
         toolbar_title.setText("Home");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.bookMedicine);
-
+        nav = findViewById(R.id.navigation);
+        item = nav.getMenu().getItem(1);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,7 +79,7 @@ public class CustomerHomePage extends BaseActivity {
             }
         });
 
-
+        item.setEnabled(false);
         spinner=findViewById(R.id.progress_loader);
         queue= Volley.newRequestQueue(this);
         createExampleList();
@@ -191,7 +194,7 @@ public class CustomerHomePage extends BaseActivity {
                                     Date result1 = df1.parse(string1);
                                     String dateString = result1.toString();
                                     Log.d("date", dateString);
-                                    currentList.add(new CustomerBookingHistoryCard(medicine.getString("name"), medicine.getString("strength"), medicine.getString("manufacturer"), shop.getString("name"), shop.getString("address"), shop.getString("phone"), dateString));
+                                    currentList.add(new CustomerBookingHistoryCard(medicine.getString("name"), medicine.getString("strength"), medicine.getString("manufacturer"), shop.getString("name"), shop.getString("address"), shop.getString("phone"), dateString , jsonObject.getString("deadline"), false));
                                 }
                             }
                         }catch (JSONException e) {
--- app/src/main/java/com/example/medconnect/SearchMedicineActivity.java
+++ app/src/main/java/com/example/medconnect/SearchMedicineActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -28,6 +29,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.navigation.NavigationView;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import org.json.JSONArray;
@@ -45,6 +47,8 @@ public class SearchMedicineActivity extends BaseActivity {
     private ArrayList<MedicineItem> medicineList;
     private ProgressBar spinner;
     private RequestQueue queue;
+    private NavigationView nav;
+    private MenuItem item;
     Utils utils;
 
     @Override
@@ -55,6 +59,9 @@ public class SearchMedicineActivity extends BaseActivity {
         toolbar_title.setText("Search Medicine");
         queue= Volley.newRequestQueue(this);
         spinner=findViewById(R.id.progress_loader);
+        nav = findViewById(R.id.navigation);
+        item = nav.getMenu().getItem(3);
+        item.setEnabled(false);
         utils=new Utils();
 
         this.createList();
